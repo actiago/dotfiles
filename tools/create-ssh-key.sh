2549ec6f21dca8ba31869df68cfb5682e2a649b9
Tiago Amaral <tiagoamaralc@gmail.com>
2020-10-16 13:09:32 -0300

    Change user

--- tools/create-ssh-key.sh
+++ tools/create-ssh-key.sh
@@ -3,4 +3,4 @@
 echo "Insira o nome da chave: "
 read KEYNAME
 
-ssh-keygen -t rsa -b 4096 -C "$KEYNAME" -f "/home/tiago/.ssh/id_rsa-$KEYNAME" -q
+ssh-keygen -t rsa -b 4096 -C "$KEYNAME" -f "/home/"$USER"/.ssh/id_rsa-$KEYNAME" -q
